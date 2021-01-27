db2c28ff5844889033b4fe7aa1f5f8c6875a3153
Дмитрий Шкатов <Dmitriy.Shkatov@waveaccess.ru>
2021-01-27 14:49:42 +0500

    Add eureka service

---
+++ com.github.linealnan.lutebackend.eureka/src/test/java/com/github/linealnan/lutebackend/eureka/com/github/linealnan/lutebackend/eureka/LuteBackendEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.linealnan.lutebackend.eureka.com.github.linealnan.lutebackend.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuteBackendEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
