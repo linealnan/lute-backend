cf045c1750af06474eea2023d3fbe40f209b7fef
Дмитрий Шкатов <Dmitriy.Shkatov@waveaccess.ru>
2021-02-08 13:55:13 +0500

    [NF] Added zuul service. Config routing to users service

---
+++ security-service/src/test/java/com/github/linealnan/lutebackend/securityservice/SecurityserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.linealnan.lutebackend.securityservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
